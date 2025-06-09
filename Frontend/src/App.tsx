07f43dcf9ee86bc53f4eb22e4da4f794c71caf22
yhuliaxl <yhuliaxl@gmail.com>
2025-06-08 23:24:21 -0500

    categorias eecha

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,6 +9,7 @@ import RegistrarUsuario from './pages/usuarios/RegistrarUsuario';
 import SenaEmpresas from './pages/entidades/SenaEmpresas';
 import Sedes from './pages/entidades/Sedes';
 import UnidadesProductivas from './pages/entidades/UnidadesProductivas';
+import Categorias from './pages/inventario/Categorias';
 import { useState } from 'react';
 import { GlobalStyles } from './components/global/navbar';
 
@@ -30,13 +31,15 @@ function App() {
       <div className="flex-grow">
         <Routes>
           <Route element={<Login />} path="/" />
-          <Route element={<Inicio isNavbarOpen={isOpen} />} path="/home" />
+          <Route element={<Inicio isNavbarOpen={isOpen} />} path="/inicio" />
           <Route element={<Usuarios isNavbarOpen={isOpen} />} path="/usuarios" />
           <Route element={<RegistrarUsuario isNavbarOpen={isOpen} />} path="/usuarios/registrar" />
           <Route element={<SenaEmpresas isNavbarOpen={isOpen} />} path="/entidades/empresas-sena" />
           <Route element={<Sedes isNavbarOpen={isOpen} />} path="/entidades/sede" />
           <Route element={<UnidadesProductivas isNavbarOpen={isOpen} />} path="/entidades/unidades-productivas" />
+          <Route element={<Categorias isNavbarOpen={isOpen} />} path="/inventario/categorias" />
         </Routes>
+
       </div>
     </>
   );
